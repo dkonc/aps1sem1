259c9bdc029f9583b428a9b5a8799bd42afa2df6
dkonc <dk2418@student.uni-lj.si>
2019-10-24 07:17:21 +0000

    Ustvarjanje

---
+++ Naloga1/Naloga1.java
@@ -0,0 +1,17 @@
+package Naloga1;
+
+import java.util.Scanner;
+
+public class Naloga1{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+
+        String velikostTabele = sc.next();
+        String[] stevilke = velikostTabele.split(",");
+		int sirina = Integer.parseInt(stevilke[0]);
+		int visina = Integer.parseInt(stevilke[1]);
+
+
+    }
+
+}
